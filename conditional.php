a88d2332bf83cc4558a32fcfeb55336e1bf02c02
George Tudor <georgebitq@gmail.com>
2022-06-15 13:19:22 +0300

    bump version and add changelog

--- conditional.php
+++ conditional.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://wordpress.org/plugins/visibility-logic-elementor
  * Author URI: https://staxwp.com
  * Author: StaxWP
- * Version: 2.3.0
+ * Version: 2.3.1
  *
  * Elementor tested up to: 3.6.4
  * Elementor Pro tested up to: 3.6.0
@@ -17,7 +17,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-define( 'STAX_VISIBILITY_VERSION', '2.3.0' );
+define( 'STAX_VISIBILITY_VERSION', '2.3.1' );
 
 define( 'STAX_VISIBILITY_FILE', __FILE__ );
 define( 'STAX_VISIBILITY_PLUGIN_BASE', plugin_basename( STAX_VISIBILITY_FILE ) );
